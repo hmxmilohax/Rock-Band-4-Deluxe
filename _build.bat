8776fa2f632ce6c675e1b27f2b82caff720fe01f
LysiX <smyler02@gmail.com>
2024-10-22 17:54:01 -0500

    highway texture refactor

--- _build.bat
+++ _build.bat
@@ -8,10 +8,15 @@ for /R "%~dp0_ark" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps4" dependenci
 for /R "%~dp0_ark" %%f in (*.script) do dependencies\dtacheck "%%f" "%~dp0dependencies\dtacheckfns"
 xcopy /q /e /y _ark\ps4 _tmpbuild
 python dependencies\gen_version.py "%~dp0_tmpbuild\dx\dx_version.dta"
+echo:Converting dtas...
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps4" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps4" 3
 for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps4" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps4" 3
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps4" del "%%f"
 for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps4" del "%%f"
+echo:Converting pngs...
+for /R "%~dp0_tmpbuild" %%f in (*.png) do IF NOT "%%~xf" == ".png_ps4" dependencies\forgetool\forgetool png2tex "%%f" "%%~dpnf.png_ps4"
+for /R "%~dp0_tmpbuild" %%f in (*.png) do IF NOT "%%~xf" == ".png_ps4" del "%%f"
+for /R "%~dp0_tmpbuild" %%f in (*.gitkeep) do del "%%f"
 xcopy /q /e /y _tmpbuild _build\GoldHEN\RB4DX\ps4\
 for /R "%~dp0_tmpbuild" %%f in (*) do del "%%f"
 rmdir /s /q "%~dp0_tmpbuild"
